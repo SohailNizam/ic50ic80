1f402c1b7d66bfa7f6faf7ca01c0dad076cd8518
Sohail Nizam <snizam@emory.edu>
2021-02-02 23:25:03 -0500

    Finished py and r scripts
    For now assuming working dir is ic50ic80 at run time

---
+++ Python/scrape_htmls.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 11:09:53 2021
+
+@author: sohailnizam
+"""
+
+#-------------------------------------------------#
+# Scrape cvR2 and CI data from slapnap html files #
+#-------------------------------------------------#
+
+
+#import necessary libraries
+import os
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+from glob import glob
+
+#the bnabs which have directories in ic50ic80/docker_output 
+#bnabs = ["vrc01", "10-1074", "10-996", "2f5", "2g12", "35o22",
+#         "3bnc117", "4e10", "8anc195", "b12", "ch01", "hj16",
+#         "nih45-46", "pg16", "pg9", "pgdm1400", "pgt121", 
+#         "pgt128", "pgt135", "pgt145", "pgt151", "vrc-ch31",
+#        "vrc-pg04", "vrc01", "vrc03", "vrc07", "vrc26.08",
+#        "vrc26.25", "vrc29.03", "vrc34.01"]
+
+bnabs = os.listdir("./docker_output")
+
+
+def scrape_tables(bnabs):
+    
+    '''
+    This function iterates over the directories of the bnabs,
+    extracts the cross validated R2 as well as the CI lb and ubs
+    for both ic50 and ic80. These values are stored in a pandas df.
+    
+    Input: list of bnab name strings
+    Output: pandas df with 2 rows per bnab (1 for ic50, 1 for ic80)
+            and 4 columns (bnab, method, r2, cil, ciu)
+    '''
+    #init empty list to hold scraped r2s, lbs, and ubs
+    cvR2_data = []
+
+    
+    for bnab in bnabs:
+        
+        #assume ic50ic80 is the current working dir
+        path = "./docker_output/"
+        path += bnab
+        #report_path = path + "/report_" + bnab.upper() + "_" + date + ".html"
+        
+        #pick out the file that starts with 'report' ends with '.html'
+        #this method is not date specific
+        report_path = glob(path + "/report*.html")[0]
+        report = open(report_path).read()
+        
+        #scrape the html with beautiful soup
+        soup = BeautifulSoup(report)
+        #extract the second table in the html
+        cvR2_table = soup.findAll("table")[1]
+        
+        #init a list to hold the data for this bnab
+        output_rows = []
+        #two rows not including header: ic50 and ic80
+        for table_row in cvR2_table.findAll("tr")[1:]:
+            
+            #three columns not including label
+            columns = table_row.findAll('td')
+            output_row = []
+            for column in columns[1:]:
+            
+                output_row.append(column.text)
+                
+            output_rows.append(output_row)
+        
+        #add the rows for this bnab to the running list
+        cvR2_data += output_rows
+        #print(bnab + " done")
+    
+    
+    #cast the data list of lists to pandas df
+    cvR2_dataframe = pd.DataFrame(cvR2_data, columns = ["r2", "cil", "ciu"])
+    
+    
+    #add nab and ic columns
+    cvR2_dataframe.insert(loc=0, column='bnab', value=np.repeat(bnabs, 2))
+    cvR2_dataframe.insert(loc=1, column='method', value=['ic50', 'ic80']*len(bnabs))
+
+
+    #return sorted to ic50 and ic80 are separate
+    return(cvR2_dataframe.sort_values(by = ['method', 'bnab']))
+        
+
+#call the function on the bnabs
+result_df = scrape_tables(bnabs)
+        
+#write to csv
+result_df.to_csv("./Python/cvr2_df.csv")
+    
+    
---
+++ R/make_r2_fig.R
@@ -0,0 +1,151 @@
+# create a figure examining cv R^2 for each bnab using ic50 and ic80 outcomes
+# similar to figure slapnap_supplemental/single_sensitivity/R/make_auc_fig.R
+
+# --------------------------------------------------------------------------
+# Set up
+# --------------------------------------------------------------------------
+# load required libraries
+library("dplyr")
+library("forcats")
+library("tibble")
+library("ggplot2")
+library("cowplot")
+theme_set(theme_cowplot())
+library("here")
+
+# create a tibble with the data
+#load(here("py_output", "cvr2_df.cvs"))
+rslt_df <- read.csv(file = "./Python/cvr2_df.csv")
+rslt_df <- rslt_df[complete.cases(rslt_df), ] #remove any NA rows
+
+
+compare_tib <- rslt_df %>%
+  as_tibble() %>%
+  mutate(
+    bnab = toupper(bnab),
+    epitope = case_when(
+      bnab %in% c("2G12", "PG16", "PG9", "PGDM1400",
+                  "PGT145", "VRC26.08", "VRC26.25") ~ "V1V2",
+      bnab %in% c("10-1074", "10-996", "DH270.1",
+                  "DH270.5", "DH270.6", "PGT121", "PGT128",
+                  "PGT135", "VRC29.03", "VRC38.01") ~ "V3",
+      bnab %in% c("3BNC117", "B12", "CH01",
+                  "HJ16", "NIH45-46", "VRC-CH31", "VRC-PG04",
+                  "VRC01", "VRC03", "VRC07") ~ "CD4bs",
+      bnab %in% c("PGT151", "VRC34.01") ~ "Fusion peptide",
+      bnab %in% c("35O22", "8ANC195") ~ "Subunit interface",
+      bnab %in% c("2F5", "4E10") ~ "MPER"
+    ))
+
+
+# --------------------------------------------------------------------------
+# Create the plot
+# --------------------------------------------------------------------------
+epitope_labs <- unique(compare_tib$epitope)[order(unique(compare_tib$epitope), decreasing = TRUE)]
+point_size <- 2
+
+cd4bs_plot <- compare_tib %>%
+  filter(epitope == "CD4bs") %>%
+  ggplot(aes(x = forcats::fct_reorder(as.factor(bnab), desc(epitope)),
+             y = r2, ymin = cil, ymax = ciu, shape = method,
+             group = paste0(epitope, "_", method))) +
+  geom_point(position = position_dodge(width = 0.75, preserve = "total"),
+             size = point_size) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  ylim(c(-.5, 1)) + #may need to update
+  ylab("CV-R2") + 
+  ggtitle("CD4bs") +
+  xlab("") +
+  #labs(y = NULL) +
+  guides(x = guide_axis(n.dodge = 2), shape = FALSE) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+fusion_plot <- compare_tib %>%
+  filter(epitope == "Fusion peptide") %>%
+  ggplot(aes(x = forcats::fct_reorder(as.factor(bnab), desc(epitope)),
+             y = r2, ymin = cil, ymax = ciu, shape = method,
+             group = paste0(epitope, "_", method))) +
+  geom_point(position = position_dodge(width = 0.75, preserve = "total"),
+             size = point_size) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  ylim(c(-.5, 1)) + #may need to update
+  labs(y = NULL) +
+  ggtitle("Fusion peptide") +
+  xlab("") +
+  guides(x = guide_axis(n.dodge = 2), shape = FALSE, y = "none") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+mper_plot <- compare_tib %>%
+  filter(epitope == "MPER") %>%
+  ggplot(aes(x = forcats::fct_reorder(as.factor(bnab), desc(epitope)),
+             y = r2, ymin = cil, ymax = ciu, shape = method,
+             group = paste0(epitope, "_", method))) +
+  geom_point(position = position_dodge(width = 0.75, preserve = "total"),
+             size = point_size) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  ylim(c(-.5, 1)) + #may need to update
+  labs(y = NULL) +
+  ggtitle("MPER") +
+  xlab("") +
+  guides(x = guide_axis(n.dodge = 2), y = "none") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+subunit_plot <- compare_tib %>%
+  filter(epitope == "Subunit interface") %>%
+  ggplot(aes(x = forcats::fct_reorder(as.factor(bnab), desc(epitope)),
+             y = r2, ymin = cil, ymax = ciu, shape = method,
+             group = paste0(epitope, "_", method))) +
+  geom_point(position = position_dodge(width = 0.75, preserve = "total"),
+             size = point_size) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  ylim(c(-.5, 1)) +
+  labs(y = NULL) +
+  ggtitle("Subunit interface") +
+  xlab("") +
+  guides(x = guide_axis(n.dodge = 2), shape = FALSE, y = "none") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+v1v2_plot <- compare_tib %>%
+  filter(epitope == "V1V2") %>%
+  ggplot(aes(x = forcats::fct_reorder(as.factor(bnab), desc(epitope)),
+             y = r2, ymin = cil, ymax = ciu, shape = method,
+             group = paste0(epitope, "_", method))) +
+  geom_point(position = position_dodge(width = 0.75, preserve = "total"),
+             size = point_size) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  ylim(c(-.5, 1)) +
+  ylab("CV-R2") +
+  ggtitle("V1V2") +
+  xlab("") +
+  guides(x = guide_axis(n.dodge = 2), shape = FALSE) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+v3_plot <- compare_tib %>%
+  filter(epitope == "V3") %>%
+  ggplot(aes(x = forcats::fct_reorder(as.factor(bnab), desc(epitope)),
+             y = r2, ymin = cil, ymax = ciu, shape = method,
+             group = paste0(epitope, "_", method))) +
+  geom_point(position = position_dodge(width = 0.75, preserve = "total"),
+             size = point_size) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  ylim(c(-.5, 1)) +
+  labs(y = NULL) +
+  ggtitle("V3") +
+  xlab("") +
+  guides(x = guide_axis(n.dodge = 2), y = "none", shape = FALSE) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+compare_plot <- plot_grid(
+  plot_grid(
+    plot_grid(v1v2_plot, v3_plot, nrow = 1),
+    plot_grid(cd4bs_plot, fusion_plot, subunit_plot, mper_plot, nrow = 1,
+              rel_widths = c(3, 1, 1, 1.5)), #may need to change
+    nrow = 2
+  ),
+  ggplot() + ggtitle("bnAb") + guides(x = "none", y = "none") +
+    theme(plot.title = element_text(hjust = 0.5, face = "plain")),
+  nrow = 2, rel_heights = c(1, 0.1)
+)
+
+
